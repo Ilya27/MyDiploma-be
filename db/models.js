fc0effce051cccb224c45ad7a55ff5cc4e6ae2eb
Alexander <a.litkevich@smissltd.com>
2020-02-19 19:08:19 +0200

    added customer registration with tests

--- db/models.js
+++ db/models.js
@@ -3,5 +3,6 @@ const config = require('../config/');
 
 module.exports = {
     Sessions: require('./Models/Sessions'),
-    Accounts: require('./Models/Accounts')
+    Accounts: require('./Models/Accounts'),
+    Addresses: require('./Models/Addresses'),
 };
