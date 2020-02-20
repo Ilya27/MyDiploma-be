c5762e7764e2247f569cf4a9f7fb662b6614d642
Boris <b.kotlyarov@smissltd.com>
2020-02-20 16:28:14 +0200

    tests infrastructure fix

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ coming soon
 
 ## Tests ##
 
- - Create file `PROJECT_DIR/.env.test` 
- - Run test: `export NODE_ENV=test && mocha --require ./specs/helpers.js --timeout 0 ./specs/**/*.spec.js`
+ - Create file `PROJECT_DIR/.env.test`
+ - Run test: `export NODE_ENV=test && mocha --require ./specs/helpers.js --timeout 0 "./specs/**/*.spec.js" --recursive`
 
--- specs/index.spec.js
+++ specs/index.spec.js
@@ -19,39 +19,8 @@ before(async function (done) {
     console.log('Drop TEST database');
     await connection.connection.db.dropDatabase();
 
-    const {Sessions, Accounts} = require('../db/models');
-
-    const testUser = await Accounts.insert({
-        login: 'admin',
-        password: 'admin',
-        email: 'test@gmail.com',
-        role: 'ADMIN',
-        stripeId: '111',
-        phoneNumber: '111',
-        status: "ACTIVE",
-    });
-
-    console.log(`user created`);
-
-    const session = new Sessions({
-        status: 'ACTIVE',
-        token: TESTS_USER_TOKEN,
-        account: testUser._id,
-        creationDate: new Date().getTime(),
-
-    });
-
-    const sessionToClose = new Sessions({
-        status: 'ACTIVE',
-        token: TESTS_TOKEN_FOR_CLOSE_SESSION,
-        account: testUser._id,
-        creationDate: new Date().getTime(),
-
-    });
-
-    await session.save();
-    await sessionToClose.save();
-    console.log(`user session created`);
+    const insertData = require('./isertData');
+    await insertData();
 
 
     await connection.disconnect();
